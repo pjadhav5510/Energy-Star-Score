c505739e26801fc0658fa6753a7980281b01afbf
Piyush Jadhav <112354938+pjadhav5510@users.noreply.github.com>
2024-04-30 11:36:04 -0700

    Update 2_Exploratory_Data_Analysis.py

--- pages/2_Exploratory_Data_Analysis.py
+++ pages/2_Exploratory_Data_Analysis.py
@@ -47,3 +47,40 @@ st.write('''After acquiring the precise column datatypes, we carry out the analy
 This function calculates the percentage of missing data and the number of missing values for each column. ''')
 st.image('Images/Picture3.png')
 st.write('We also delt with missing data in columns, we removed 11 columns which had missing data in it.')
+
+st.header(':blue[Data Visualizations]', divider='red')
+
+col1, col2 = st.columns(2)
+col3, col4 = st.columns(2)
+
+with st.container():
+    with col1:
+        #st.text('')
+        st.image('Images/scores.png')
+        st.text('')
+        st.write('Our first plot has already revealed some surprising (and suspicious) information! As the Energy Star Score is a percentile rank, we would expect to see a completely flat distribution with each score making up 1% of the distribution (about 90 buildings).')
+
+    with col2:
+        st.image('Images/EUI.png')
+        st.text('')
+        st.write('This plot looks a little less suspicious and is close to normally distributed with a long tail on the right side (it has a positive skew).')
+
+with st.container():
+    
+    with col3:
+        st.text('')
+
+        st.image('Images/density.png')
+        st.text('')
+        st.write('From this graph, we can see that the building type does have an effect on the score (the negative scores on the graph are an artifact of the kernel density estimation procedure).')
+
+
+    with col4:
+        st.text('')
+        st.image('Images/density_borough.png')
+        st.text('')
+        st.write('The borough of the building does not seem to make as significant a difference in the distribution of the score as does the building type.')
+
+st.image('Images/parisplot.png',use_column_width ='always')
+st.text('')
+st.write('')
